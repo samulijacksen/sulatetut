74c4df0b10677281b6c3edd688e44133c7f4d359
samulijacksen <69836069+samulijacksen@users.noreply.github.com>
2020-08-18 11:31:36 +0300

    20200818

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Header from './components/Header';
 
 function App() {
+  const initjobs = [
+    {
+      "id": 1,
+      "tyotehtava": "lastenhoitaja"
+    },
+    {
+      "id": 2,
+      "tyotehtava": "lakaisukoneen kuljettaja"
+    },
+    {
+      "id": 3,
+      "tyotehtava": "alepankassa"
+    }
+  ]
+  const [jobs, setJobs] =useState(initjobs)
+  const rows = () => jobs.map(job => {
+    return <p>{job.tyotehtava}</p>
+  })
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      {rows()}
     </div>
   );
 }
