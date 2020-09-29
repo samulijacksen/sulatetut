d4c42b8b5344ba2edd1265959d1cb00e74624659
samulijacksen <69836069+samulijacksen@users.noreply.github.com>
2020-09-29 11:31:53 +0300

    20200929

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,11 +1,14 @@
 import React, { useState } from 'react'
-import { VictoryChart, VictoryLine, VictoryHistogram} from 'victory'
+import { VictoryChart, VictoryLine, VictoryGroup, VictoryBar, VictoryPortal, VictoryLabel } from 'victory'
 
 function Weather() {
 
+  function convertUTCDateToLocalDate(date) {
+    new Date(date.getTime() + date.getTimezoneOffset() * 60 * 1000);
+    return date;
+  }
   const today = new Date();
   const date = today.getDate() + "." + parseInt(today.getMonth() + 1) + "." + today.getFullYear();
-
   const initWeather = [];
   const [weather, setWeather] = useState(initWeather);
 
@@ -16,19 +19,20 @@ function Weather() {
   let humtempkey = 1;
   let chartTempData = [];
   let chartHumData = [];
+
   const rows = () => weather.slice(0, 24).reverse().map(temphum => {
-    const measurmentDate = temphum.PublishedAt.split('T')[0].split('-')[2] + '.' + temphum.PublishedAt.split('T')[0].split('-')[1] + '.' + temphum.PublishedAt.split('T')[0].split('-')[0]
-    const measurmentTime = temphum.PublishedAt.split('T')[1] + ('.') + temphum.PublishedAt.split(':')[0] + temphum.PublishedAt.split(':')[0] + temphum.PublishedAt.split(':')[1] + temphum.PublishedAt.split(':')[1]
-    chartTempData.push({ x: String(measurmentTime), y: temphum.Temp });
-    chartHumData.push({ experiment: String(measurmentTime), actual: parseInt(temphum.hum), label: String(temphum.hum) });
-    return <div key={humtempkey++}> <b>Pvm</b>{measurmentDate},<b>Klo</b>{measurmentTime}, {temphum.PublishedAt}--------<b>Ilmankosteus</b>{temphum.Hum.split('.')[0]}%--------<b>Lämpötila</b>{temphum.Temp.split('.')[0]}°C </div>
+    const fixedTime = String(convertUTCDateToLocalDate(new Date(temphum.PublishedAt)));
+    const time = fixedTime.split(' ')[4].split(':')[0] + '.' + fixedTime.split(' ')[4].split(':')[1];
+    const measurmentDate = temphum.PublishedAt.split('T')[0].split('-')[2] + '.' + temphum.PublishedAt.split('T')[0].split('-')[1] + '.' + temphum.PublishedAt.split('T')[0].split('-')[0];
+    const measurmentTime = temphum.PublishedAt.split('T')[1].split(':')[0] + ('.') + temphum.PublishedAt.split('T')[1].split(':')[1];
+    chartHumData.push({ x: String(time), y: parseInt(temphum.Hum), label: String(temphum.Hum) });
+    chartTempData.push({ experiment: String(time), actual: parseInt(temphum.Temp) });
+    return <div key={humtempkey++}> <b>Pvm</b>{measurmentDate} <b>Klo </b>{time}--------<b>Ilmankosteus</b>{temphum.Hum.split('.')[0]}%--------<b>Lämpötila</b>{temphum.Temp.split('.')[0]}°C </div>
   })
 
   const showTemperature = chartTempData;
-  const Showhumidity = chartHumData;
-
+  const showHumidity = chartHumData;
   return (
-
     <div aling="middle">
       <div>
         <h3>P-PLOCC Lämpötila</h3>
@@ -40,21 +44,12 @@ function Weather() {
         {rows()}
       </div>
 
-
-
       <VictoryChart
         domainPadding={{ x: 30, y: 10 }}
         width={1000}
         height={250}>
         <VictoryLine
-          data={[
-            { experiment: "1.1.", actual: 10 },
-            { experiment: "2.1.", actual: 5 },
-            { experiment: "3.1.", actual: 0 },
-            { experiment: "4.1.", actual: -10 },
-            { experiment: "5.1.", actual: -5 },
-            { experiment: "6.1.", actual: -0 }
-          ]}
+          data={showTemperature}
           style={{
             data:
               { stroke: "green", strokeWidth: 2 }
@@ -63,35 +58,21 @@ function Weather() {
           y="actual" />
       </VictoryChart>
 
-      <VictoryChart>
-        <VictoryHistogram
-          style={{
-            data:
-              { fill: '#F1737F' }
-          }}
-          cornerRadius={3}
-          data={[
-            { x: 10 },
-            { x: 10 },
-            { x: 10 },
-            { x: 10 },
-            { x: 10 },
-            { x: 1 },
-            { x: 2 },
-            { x: 3 },
-            { x: 4 },
-            { x: 7 },
-            { x: 17 },
-            { x: 12 }
-          ]}
-        />
+      <div aling="middle"></div>
+      <h4>P-PLOCC Lämpötila</h4>
+      <VictoryChart domainPadding={{ x: 60 }} height={350} width={1400}>
+        <VictoryGroup>
+          <VictoryBar
+            style={{
+              labels: { fontSize: 15, fill: "red" }
+            }}
+            labelComponent={<VictoryPortal><VictoryLabel /></VictoryPortal>}
+            data={showHumidity}
+          />
+        </VictoryGroup>
       </VictoryChart>
-
     </div >
-
-
-
   )
-
 }
 export default Weather;
+
